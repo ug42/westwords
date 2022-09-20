4939c1f54ea28ca53cfd5742058422f57c0a517c
Matt Westover <oddymaw@gmail.com>
2022-09-20 10:20:11 -0700

    Update the Procfile with right app name (again) Add some add'l socket types and test game "interactions"

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --worker-class eventlet -w 1 server:APP
+web: gunicorn --worker-class eventlet -w 1 server:app
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, ConnectionRefusedError
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -12,13 +12,15 @@ config = {
 
 @app.route('/')
 def game():
-    render_template('game.html')
+    return render_template('game.html')
 
 
 @socketio.on('connect')
 def connect(auth):
     emit('my response', {'data': 'Connected'})
-    print('Auth details: ' + str(auth))
+    print('Client connected')
+    if auth:
+        print('Auth details: ' + str(auth))
 
 
 @socketio.on('disconnect')
@@ -26,7 +28,7 @@ def disconnect():
     print('Client disconnected')
 
 
-@socket.on('question')
+@socketio.on('question')
 def question(json):
     print('received question: ' + str(json))
     emit('mayor question', json, broadcast=True)
