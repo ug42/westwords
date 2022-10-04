78361f956dade2cb7457018e71752346dcb33975
ug42 <oddymaw@gmail.com>
2022-10-03 22:36:37 -0700

    Factor out the jquery bits

--- static/js/script.js
+++ static/js/script.js
@@ -1,36 +1,55 @@
-$(document).ready(function () {
+function ready(fn) {
+    if (document.readyState !== 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+ready(function () {
+    // Side bar click-to-hide stuff
+    function hideSidebar() {
+        document.getElementById('openSideMenu').checked = false;
+    }
+    var sideIconToggle = document.getElementById('sidebarContainer');
+    document.addEventListener('click', function (event) {
+        if (!sidebarContainer.contains(event.target))
+            hideSidebar();
+    });
+    // TODO: Factor out jquery
     // TODO: update all defaultgame references
     // TODO: update all default_game_state to be the local_game_state after
     //       emitting the correct values form server
     // TODO: add separate sockets for each of the game comms
     var socket = io.connect({ autoconnect: true });
-    // let question = document.getElementById('question');
+    let question = document.getElementById('question');
 
     var local_game_state = {};
     var default_game_state = { 'game_state': 'SETUP', 'players': [], 'time': 300 };
 
-    var timer = null;
+    var timer;
+    game_timer = document.getElementById('game_timer');
     reset_game_timer(default_game_state['time']);
     function reset_game_timer(seconds) {
         timer = new easytimer.Timer({
             countdown: true,
             startValues: { seconds: default_game_state['time'] }
         });
-        $('#game_timer').html(timer.getTimeValues().toString());
+        game_timer.innerHtml = timer.getTimeValues().toString();
         timer.addEventListener('secondsUpdated', function (e) {
-            $('#game_timer').html(timer.getTimeValues().toString());
+            game_timer.innerHtml = timer.getTimeValues().toString();
         });
         timer.addEventListener('targetAchieved', function (e) {
-            $('#game_timer').html('KABOOM!!');
+            game_timer.innerHtml = 'KABOOM!!';
         });
     }
 
     function print_game_state_html(game_state) {
-        players = ""
+        var players = ""
         for (var i = 0; i < game_state['players'].length; i++) {
             players = players + '<br>' + game['players'][i];
         };
-        questions = ""
+        var questions = ""
         for (var i = 0; i < game_state['questions'].length; i++) {
             questions = questions + '<br><div id="q' + i + '>' + game['questions'][i] + '</div>';
         };
@@ -48,57 +67,63 @@ $(document).ready(function () {
     });
     socket.on('game_state', function (game_state) {
         local_game_state = game_state;
-        $('#game_status').html(print_game_state_html());
+        document.getElementById('game_status').innerHtml(print_game_state_html);
         console.log(game_state);
     });
     socket.on('pause', function (time) {
         reset_game_timer(time);
         console.log('Pausing timer with ' + time + ' seconds left.');
     });
-
-    $('#question_submit').on('click', function () {
+    
+    document.getElementById('question_submit').addEventListener('click', function () {
         console.log('emitting question: ' + question.value);
         if (question.value != "") {
             socket.emit('question', question.value);
         }
     });
-    $('#game_start').on('click', game_start_resume);
-    $('#game_pause').on('click', game_pause);
-    $('#game_reset').on('click', game_reset);
-    $('#get_game_state').on('click', get_game_state);
-    $('#proper_noun').on('click', function () {
+
+    var game_start_btn = document.getElementById('game_start');
+    var game_pause_btn = document.getElementById('game_pause');
+    var game_reset_btn = document.getElementById('game_reset');
+    var get_game_state_btn = document.getElementById('get_game_state');
+    var proper_noun_btn = document.getElementById('proper_noun');
+    game_start_btn.addEventListener('click', game_start_resume);
+    game_pause_btn.addEventListener('click', game_pause);
+    game_reset_btn.addEventListener('click', game_reset);
+    get_game_state_btn.addEventListener('click', get_game_state);
+    proper_noun_btn.addEventListener('click', function () {
         socket.emit('question', "Is it a proper noun?");
     });
     function game_start_resume() {
         console.log('Attempting to start game');
         socket.emit('game_start', 'defaultgame');
         timer.start();
-        $('#game_start').html('Start game');
-        $('#game_start').prop("disabled", true);
-        $('#game_reset').prop("disabled", true);
-        $('#game_pause').prop("disabled", false);
-        $('#proper_noun').prop("disabled", false);
+        game_start_btn.innerText = 'Start game';
+        game_start_btn.disabled = true;
+        game_reset_btn.disabled = true;
+        game_pause_btn.disabled = false;
+        proper_noun_btn.disabled = false;
     };
     function game_pause() {
         console.log('Attempting to pause game');
         socket.emit('game_pause', 'defaultgame');
         timer.pause();
-        $('#game_start').html('Resume game');
-        $('#game_start').prop("disabled", false);
-        $('#game_reset').prop("disabled", false);
-        $('#game_pause').prop("disabled", true);
-        $('#proper_noun').prop("disabled", true);
+        game_start_btn.innerText = 'Resume game';
+        game_start_btn.disabled = false;
+        game_reset_btn.disabled = false;
+        game_pause_btn.disabled = true;
+        proper_noun_btn.disabled = true;
     };
     function game_reset() {
         console.log('Attempting to reset game');
         socket.emit('game_reset', 'defaultgame');
         // reset_game_timer(default_game_state['time']);
         reset_game_timer(60);
-        $('#game_start').html('Start game');
-        $('#game_start').prop("disabled", false);
-        $('#game_reset').prop("disabled", false);
-        $('#game_pause').prop("disabled", true);
-        $('#proper_noun').prop("disabled", true);
+        game_start_btn.innerText = 'Start game';
+        game_start_btn.disabled = false;
+        game_reset_btn.disabled = false;
+        game_pause_btn.disabled = true;
+        proper_noun_btn.disabled = true;
     };
     function get_game_state() {
         socket.emit('get_game_state', 'defaultgame');
@@ -106,7 +131,7 @@ $(document).ready(function () {
     function parse_game_state(game_state) {
         if (default_game_state['game_state'] == 'STARTED') {
             reset_game_timer(default_game_state);
-            $('#game_timer').timer('start');
+            game_timer.timer('start');
         };
     };
     // TODO: make this so it puts the most recent question at the top...
